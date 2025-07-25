53b2d3938090bfbab452d45889e2b3593b8a1c8d
Dadarao Raut <115204873+DadaraoRaut@users.noreply.github.com>
2025-07-25 15:13:50 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,8 @@
 def call(String imageName) {
-    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDENTIALS', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh '''
-            echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-        '''
-        sh "docker push ${imageName}"
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        sh """
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            docker push ${imageName}
+        """
     }
 }
